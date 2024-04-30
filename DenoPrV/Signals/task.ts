39aaabe25be028cc626acf3ea4a0f4addee693f8
aanconao <alan_ancona_ortiz@hotmail.com>
2024-04-30 20:40:01 +0200

    deleteComponentsEditIslans

---
+++ DenoPrV/Signals/task.ts
@@ -0,0 +1,4 @@
+import { signal } from "@preact/signals";
+import { Create_Task } from "../Types.ts";
+
+export const Task = signal<Create_Task[]>;
--- DenoPrV/islands/Page.tsx
+++
@@ -1,8 +0,0 @@
-import { FunctionComponent } from "preact";
-import To_Do_State from "../components/To_do.tsx";
-import { useSignal } from "@preact/signals";
-
-const Page: FunctionComponent = () => {
-  const To_Do_signal = useSignal<>;
-  return <To_Do_State />;
-};
