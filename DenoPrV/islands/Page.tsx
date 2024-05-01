d00472de0b5bbbc4ae649cf378aa610706bff709
aanconao <alan_ancona_ortiz@hotmail.com>
2024-05-01 15:12:11 +0200

    comm

--- DenoPrV/islands/Page.tsx
+++ DenoPrV/islands/Page.tsx
@@ -1,14 +1,18 @@
 import { FunctionComponent } from "preact";
 import { useSignal } from "@preact/signals";
 import { useEffect, useState } from "preact/hooks";
-import { Create_Task } from "../Types.ts";
+import { Create_Task, States } from "../Types.ts";
 
 const Page: FunctionComponent = () => {
   const [task, setTask] = useState<Create_Task[]>([]);
-  useEffect(() => {}, []);
+
   return (
     <div>
-      <button>Crear</button>
+      <button class="buttonCreate">Create</button>
+
+      <div class="Rectangle">To Do</div>
     </div>
   );
 };
+
+export default Page;
--- DenoPrV/routes/index.tsx
+++ DenoPrV/routes/index.tsx
@@ -1,9 +1,7 @@
 import { useSignal } from "@preact/signals";
+import Page from "../islands/Page.tsx";
 
 export default function Home() {
   const count = useSignal(3);
-  return (
-    <div>
-    </div>
-  );
+  return <Page />;
 }
