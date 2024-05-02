2737bfeef033340ce5682fdad728c69afd71d599
aanconao <alan_ancona_ortiz@hotmail.com>
2024-05-02 16:24:26 +0200

    FINAL

--- DenoPrV/islands/Page.tsx
+++ DenoPrV/islands/Page.tsx
@@ -4,23 +4,55 @@ import FormButtonComponent from "../components/FormButtonComponent.tsx";
 import { Task, TaskStates } from "../Types.ts";
 
 const Page: FunctionComponent = () => {
+  const [tasksCreated, setTaskCreated] = useState<Task[]>([]);
+  const taskListt = (newTask: Task) => {
+    setTaskCreated([...tasksCreated, newTask]);
+  };
   return (
     <div class="boxPage">
       <div class="buttonForm">
-        <FormButtonComponent propTask={(newTask: Task) => {}} />
+        <FormButtonComponent propTask={taskListt} />
       </div>
       <div class="spaceBtwColumns">
         <div class="column">
           <h2>To Do</h2>
+          <ul>
+            {tasksCreated
+              .filter((tsk) => tsk.states === TaskStates.TO_DO)
+              .map((e) => (
+                <li>{e.nameTask}</li>
+              ))}
+          </ul>
         </div>
         <div class="column">
           <h2>In Progress</h2>
+          <ul>
+            {tasksCreated
+              .filter((tsk) => tsk.states === TaskStates.IN_PROGRESS)
+              .map((e) => (
+                <li>{e.nameTask}</li>
+              ))}
+          </ul>
         </div>
         <div class="column">
           <h2>In Review</h2>
+          <ul>
+            {tasksCreated
+              .filter((tsk) => tsk.states === TaskStates.IN_REVIEW)
+              .map((e) => (
+                <li>{e.nameTask}</li>
+              ))}
+          </ul>
         </div>
         <div class="column">
           <h2>Done</h2>
+          <ul>
+            {tasksCreated
+              .filter((tsk) => tsk.states === TaskStates.DONE)
+              .map((e) => (
+                <li>{e.nameTask}</li>
+              ))}
+          </ul>
         </div>
       </div>
     </div>
